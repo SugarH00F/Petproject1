4fe2ab74853c050a5ca0cc0633ed701dd15d1ca3
SugarH00F <egorov3211@gmail.com>
2023-02-02 15:25:54 +0300

    make .sh script

---
+++ make_site.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+cd $HOME
+sudo apt install -y git
+sudo git clone -b https://github.com/AnnaConda007/testForAlex.git
+cd testForAlex
+mv * /var/www/html/
---
+++ scripts/install web.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Update the apt package index
+sudo apt-get update
+
+# Install Apache2 package
+sudo apt-get install apache2 -y
